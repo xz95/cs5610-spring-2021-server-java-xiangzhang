f10a554a2274eba4d837067241703db4c93e7aa0
Xiang Zhang <xz95@uw.edu>
2021-02-09 23:26:10 -0800

    reorg user-admin js code; fixed updateUser bug

--- wbdv-sp21-01-xiangzhang-server-java/src/main/webapp/services/user.service.client.js
+++ wbdv-sp21-01-xiangzhang-server-java/src/main/webapp/services/user.service.client.js
@@ -1,11 +1,11 @@
 function AdminUserServiceClient() {
   this.createUser = createUser;
   this.findAllUsers = findAllUsers;
-  this.findUserById = findUserById;
   this.deleteUser = deleteUser;
   this.updateUser = updateUser;
   this.url = 'https://wbdv-generic-server.herokuapp.com/api/001832331/users';
   var self = this;
+
   function createUser(user) {
     return fetch(self.url, {
       method: 'POST',
@@ -17,6 +17,7 @@ function AdminUserServiceClient() {
       return response.json()
     })
   }
+
   function findAllUsers() {
     // fetch returns an object
     var promise = fetch(self.url)
@@ -25,9 +26,7 @@ function AdminUserServiceClient() {
       return response.json()
     })
   }
-  function findUserById(userId) {
 
-  }
   function updateUser(userId, user) {
     return fetch(`${self.url}/${userId}`,
         {method: 'PUT',
@@ -39,6 +38,7 @@ function AdminUserServiceClient() {
         {mode: 'cors'}
         ).then(response => response.json())
   }
+
   function deleteUser(userId) {
     return fetch(`${self.url}/${userId}`,
         {method: 'DELETE'},
--- wbdv-sp21-01-xiangzhang-server-java/src/main/webapp/user-admin/user-admin.controller.client.js
+++ wbdv-sp21-01-xiangzhang-server-java/src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,204 +1,146 @@
-// (function () {
-//   var users = [
-//     {username: "New User",
-//       password: "0000",
-//       firstName: "first name",
-//       lastName: "last name",
-//       role: "Faculty"
-//     },
-//     {username: "New User",
-//       password: "1111",
-//       firstName: "first name",
-//       lastName: "last name",
-//       role: "Faculty"
-//     }
-//   ];
+(function () {
 
-// var $usernameFld, $passwordFld;
-// var $firstNameFld, $lastNameFld, $roleFld;
-// var $removeBtn, $editBtn, $createBtn;
-// var $userRowTemplate, $tbody;
-// var $addCourse = jQuery("#addCourse")
-//var userService = new AdminUserServiceClient();
-// $(main);
-//
-//
-// function main() {
-//   renderUsers()
-// }
-//  function createUser() {
-//
-// }
-// function deleteUser() { … }
-// function selectUser() { … }
-// function updateUser() { … }
-//    function renderUsers(users) {
-//     $tbody = jQuery("tbody");
-//     console.log(users.length)
-//     for (var i = 0; i < users.length; i++) {
-//       var user = users[i]
-//       $tbody
-//           .prepend(`
-//           <tr>
-//             <td>${user.username}</td>
-//             <td>${user.password}</td>
-//             <td>${user.firstName}</td>
-//             <td>${user.lastName}</td>
-//             <td>${user.role}</td>
-//           </tr>
-//           `)
-//     }
-//    }
-//   // function findAllUsers() { … } // optional - might not need this
-//   // function findUserById() { … } // optional - might not need this
-// })();
+var $removeBtn, $editBtn, $createBtn;
+var $userRowTemplate, $tbody;
 
-// var users = [
-//   {
-//     username: "A",
-//     password: "0000",
-//     firstName: "a",
-//     lastName: "a",
-//     role: "Faculty"
-//   },
-//   {
-//     username: "B",
-//     password: "1111",
-//     firstName: "b",
-//     lastName: "b",
-//     role: "Faculty"
-//   }
-// ];
-var users = []
-var defaultNewUser = {
-  username: "New User",
-  password: "0000",
-  firstName: "first name",
-  lastName: "last name",
-  role: "Faculty"
-}
-var $addCourseBtn
-var $usernameFld
-var $passwordFld
-var $firstNameFld
-var $lastNameFld
-var $roleFld
-var $updateBtn
-var $tableBody
-var userService = new AdminUserServiceClient()
 
-function main() {
-  alert("dom has loaded")
-  $addCourseBtn = jQuery("#wbdv-addUserBtn")
-  $usernameFld = jQuery("#usernameFld")
-  $passwordFld = jQuery("#passwordFld")
-  $firstNameFld = jQuery("#firstNameFld")
-  $lastNameFld = jQuery("#lastNameFld")
-  $roleFld = jQuery("#roleFld")
-  $updateBtn = jQuery("#wbdv-updateUserBtn")
-  $tableBody = jQuery("tbody");
 
-  $addCourseBtn.click(addUser)
-  $updateBtn.click(updateUser)
+  var users = []
 
-  //createUser(defaultNewUser)
-  // fetch all users from Json
-  userService.findAllUsers()
-            .then(function (actualUsersFromServer) {
-              users = actualUsersFromServer
-              renderUsers(users)
-            })
-}
+  var $addCourseBtn
+  var $usernameFld
+  var $passwordFld
+  var $firstNameFld
+  var $lastNameFld
+  var $roleFld
+  var $updateBtn
+  var $deleteBtn
+  var $tableBody
+  var userService = new AdminUserServiceClient()
 
-function createUser(user) {
-  userService.createUser(user)
-              .then(function (actualUser) {
-    users.push(user)
-    renderUsers(users)
-  })
-}
 
-function renderUsers(users) {
+$(main);
 
-  $tableBody.empty()
-  for (var i = 0; i < users.length; i++) {
-    var user = users[i]
-    $tableBody
-    .prepend(`
+
+  function main() {
+    $addCourseBtn = jQuery("#wbdv-addUserBtn")
+    $usernameFld = jQuery("#usernameFld")
+    $passwordFld = jQuery("#passwordFld")
+    $firstNameFld = jQuery("#firstNameFld")
+    $lastNameFld = jQuery("#lastNameFld")
+    $roleFld = jQuery("#roleFld")
+    $updateBtn = jQuery("#wbdv-updateUserBtn")
+    $tableBody = jQuery("tbody");
+
+    $addCourseBtn.click(addUser)
+    $updateBtn.click(updateUser)
+
+    //createUser(defaultNewUser)
+    // fetch all users from Json
+    userService.findAllUsers()
+    .then(function (actualUsersFromServer) {
+      users = actualUsersFromServer
+      renderUsers(users)
+    })
+  }
+
+  function createUser(user) {
+    userService.createUser(user)
+    .then(function (actualUser) {
+      users.push(actualUser)
+      renderUsers(users)
+    })
+    resetToDefaultValue()
+  }
+
+  function deleteUser(event) {
+    $deleteBtn = jQuery(event.target)
+    var theIndex = $deleteBtn.attr("id")
+    var theId = users[theIndex]._id
+
+    userService.deleteUser(theId)
+    .then(function (status) {
+      users.splice(theIndex, 1)
+      renderUsers(users)
+    })
+
+  }
+
+  var selectedUser = null
+  function selectUser(event) {
+    var selectBtn = jQuery(event.target)
+    var theId = selectBtn.attr("id")
+    selectedUser = users.find(user => user._id === theId)
+    $usernameFld.val(selectedUser.username)
+    $passwordFld.val(selectedUser.password)
+    $firstNameFld.val(selectedUser.firstName)
+    $lastNameFld.val(selectedUser.lastName)
+    $roleFld.val(selectedUser.role)
+  }
+
+  function updateUser() {
+    selectedUser.username = $usernameFld.val()
+    selectedUser.password = $passwordFld.val()
+    selectedUser.firstName = $firstNameFld.val()
+    selectedUser.lastName = $lastNameFld.val()
+    selectedUser.role = $roleFld.val()
+    userService.updateUser(selectedUser._id, selectedUser)
+    .then(function (status) {
+      var index = users.findIndex(user => user._id === selectedUser._id)
+      users[index] = selectedUser
+      renderUsers(users)
+    })
+    //selectedUser = null
+    resetToDefaultValue()
+  }
+
+  function resetToDefaultValue() {
+    $usernameFld.val("")
+    $passwordFld.val("")
+    $firstNameFld.val("")
+    $lastNameFld.val("")
+    $roleFld.val("FACULTY")
+  }
+
+  function addUser() {
+    var newUser = {
+      username: $usernameFld.val(),
+      password: $passwordFld.val(),
+      firstName: $firstNameFld.val(),
+      lastName: $lastNameFld.val(),
+      role: $roleFld.val()
+    }
+    createUser(newUser)
+  }
+
+  function renderUsers(users) {
+    console.log(users)
+    $tableBody.empty()
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i]
+      $tableBody
+      .prepend(`
           <tr>
             <td>${user.username}</td>
-            <td>${user.password}</td>
+            <td>****</td>
+            
             <td>${user.firstName}</td>
             <td>${user.lastName}</td>
             <td>${user.role}</td>
             <td class="wbdv-actions">
             <span class="pull-right">
-              <i class="fa-2x fa fa-times wbdv-remove" id="${i}"></i>
-              <i class="fa-2x fa fa-pencil wbdv-edit" id="${user._id}"></i>
+              <i class="fa-2x fa fa-times wbdv-remove float-right" id="${i}"></i>
+              <i class="fa-2x fa fa-pencil wbdv-edit mr-2 float-right" id="${user._id}"></i>
             </span>
             </td>
           </tr> 
           `)
+    }
+    jQuery(".wbdv-remove")
+    .click(deleteUser)
+    jQuery(".wbdv-edit")
+    .click(selectUser)
   }
-  jQuery(".wbdv-remove")
-  .click(deleteUser)
-  jQuery(".wbdv-edit")
-  .click(selectUser)
-};
+})();
 
-function deleteUser(event) {
-  var deleteBtn = jQuery(event.target)
-  var theIndex = deleteBtn.attr("id")
-  var theId = users[theIndex]._id
-
-  userService.deleteUser(theId)
-              .then(function (status) {
-                users.splice(theIndex, 1)
-                renderUsers(users)
-              })
-
-}
-
-var selectedUser = null
-function selectUser(event) {
-  var selectBtn = jQuery(event.target)
-  var theId = selectBtn.attr("id")
-  selectedUser = users.find(user => user._id === theId)
-  $usernameFld.val(selectedUser.username)
-  $passwordFld.val(selectedUser.password)
-  $firstNameFld.val(selectedUser.firstName)
-  $lastNameFld.val(selectedUser.lastName)
-  $roleFld.val(selectedUser.role)
-}
-
-
-function updateUser() {
-  selectedUser.username = $usernameFld.val()
-  selectedUser.password = $passwordFld.val()
-  selectedUser.firstName = $firstNameFld.val()
-  selectedUser.lastName = $lastNameFld.val()
-  selectedUser.role = $roleFld.val()
-  userService.updateUser(selectedUser._id, selectedUser)
-            .then(function (status) {
-              var index = users.findIndex(user => user._id === selectedUser._id)
-              users[index] = selectedUser
-              renderUsers(users)
-            })
-
-}
-
-
-
-function addUser() {
-  var newUser = {
-    username: $usernameFld.val(),
-    password: $passwordFld.val(),
-    firstName: $firstNameFld.val(),
-    lastName: $lastNameFld.val(),
-    role: $roleFld.val()
-  }
-  createUser(newUser)
-}
-
-jQuery(main)
 
